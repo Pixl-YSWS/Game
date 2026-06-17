bb975ae896e953d212db673da32d73eec2ad7a7a
Ridit <riditjangra09@gmail.com>
2026-06-17 15:41:19 +0530

    chore: remove everything :D

--- maps/Houses.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Houses" tilewidth="16" tileheight="16" tilecount="252" columns="42">
- <image source="../public/assets/cozy-towns/Housing/Exterior/Houses.png" width="672" height="96"/>
-</tileset>
